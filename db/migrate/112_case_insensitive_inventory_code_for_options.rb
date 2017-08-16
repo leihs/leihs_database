40b110e07b149383336eca6ba46d388e1213056c
Thomas Schank <DrTom@schank.ch>
2017-08-16 12:26:03 +0200

    Resilient case_insensitive migrations

--- db/migrate/112_case_insensitive_inventory_code_for_options.rb
+++ db/migrate/112_case_insensitive_inventory_code_for_options.rb
@@ -1,6 +1,23 @@
 class CaseInsensitiveInventoryCodeForOptions < ActiveRecord::Migration
+  class MigrationOption < ActiveRecord::Base
+    self.table_name = 'options'
+  end
+
   def up
+    MigrationOption.where("inventory_code IS NULL").each_with_index do |mo, idx|
+      mo.update_attributes! inventory_code: sprintf("%05d",idx)
+    end
+
+    MigrationOption.select("lower(inventory_code) AS lic") \
+      .group("lower(inventory_code)").having("count(*) > 1").map(&:lic).each do |lic|
+      MigrationOption.where("lower(inventory_code) = ?", lic).each_with_index do |mo,idx|
+        mo.update_attributes! inventory_code: "#{mo.inventory_code}_#{idx}"
+      end
+    end
+
     execute <<-SQL
+      ALTER TABLE options ALTER COLUMN inventory_code SET NOT NULL;
+      ALTER TABLE options ALTER COLUMN inventory_code SET default uuid_generate_v4()::text;
       CREATE UNIQUE INDEX case_insensitive_inventory_code_for_options
         ON options (lower(inventory_code));
     SQL
