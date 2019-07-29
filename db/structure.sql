83687804763ec976d8331dad76fafeff95bd5570
Matus Kmit <matus.kmit@zhdk.ch>
2019-07-29 12:11:02 +0200

    change attachments field permissions

--- db/structure.sql
+++ db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.7
--- Dumped by pg_dump version 10.7
+-- Dumped from database version 10.9
+-- Dumped by pg_dump version 10.9
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,6 +12,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -4952,6 +4953,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('519'),
 ('520'),
 ('521'),
+('522'),
 ('6'),
 ('7'),
 ('8'),
