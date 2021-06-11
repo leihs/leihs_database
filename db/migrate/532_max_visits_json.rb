d85f8122c9e2eb572148ab26a323a1701210e5e5
Thomas Schank <DrTom@schank.ch>
2021-06-11 21:55:29 +0200

    Fix migration 532
    * Error: can't modify frozen string

--- db/migrate/532_max_visits_json.rb
+++ db/migrate/532_max_visits_json.rb
@@ -7,9 +7,9 @@ class MaxVisitsJson < ActiveRecord::Migration[5.0]
 
     Workday.where.not(max_visits: nil).in_batches do |workdays|
       workdays.each do |workday|
-        max_visits = YAML.load(workday.max_visits).try do |m| 
+        max_visits = YAML.load(workday.max_visits).try do |m|
           m.map do |k, v|
-            [k, v.to_s.force_encoding('utf-8').presence]
+            [k, v.to_s.dup.force_encoding('utf-8').presence]
           end.compact.to_h
         end
         workday.update_columns max_visits_json: max_visits
