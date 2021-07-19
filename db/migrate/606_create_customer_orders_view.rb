154e5e2b56e28a6a8ba42e6793bdf02aca63d475
Matus Kmit <matuskmit1@gmail.com>
2021-07-19 12:43:49 +0200

    state column in view

--- db/migrate/606_create_customer_orders_view.rb
+++ db/migrate/606_create_customer_orders_view.rb
@@ -2,9 +2,11 @@ class CreateCustomerOrdersView < ActiveRecord::Migration[5.0]
   def up
     execute <<~SQL
       CREATE OR REPLACE VIEW unified_customer_orders AS
+        -- contracts without orders
         SELECT uuid_generate_v5(uuid_ns_dns(), 'customer_order_' || cs.id::text) AS id,
                cs.user_id,
                cs.purpose,
+               ARRAY['APPROVED'] AS state,
                cs.created_at,
                cs.updated_at,
                NULL AS title,
@@ -19,29 +21,33 @@ class CreateCustomerOrdersView < ActiveRecord::Migration[5.0]
           WHERE rs2.contract_id = cs.id AND rs2.order_id IS NOT NULL
         )
         UNION
+        -- hand overs
         SELECT uuid_generate_v5(uuid_ns_dns(), 'customer_order_' || rs.user_id::text || rs.inventory_pool_id::text) AS id,
                rs.user_id,
                NULL AS purpose,
-               min(rs.created_at) AS created_at,
-               max(rs.updated_at) AS updated_at,
+               ARRAY['APPROVED'] AS state,
+               MIN(rs.created_at) AS created_at,
+               MAX(rs.updated_at) AS updated_at,
                NULL AS title,
                FALSE AS lending_terms_accepted,
                NULL AS contact_details,
-               array_agg(rs.id) AS reservation_ids,
+               ARRAY_AGG(rs.id) AS reservation_ids,
                NULL AS origin_table
         FROM reservations AS rs
         WHERE rs.order_id IS NULL AND rs.contract_id IS NULL
         GROUP BY rs.user_id, rs.inventory_pool_id
         UNION
+        -- customer orders
         SELECT customer_orders.id,
                customer_orders.user_id,
                customer_orders.purpose,
+               ARRAY_AGG(DISTINCT UPPER(COALESCE(orders.state, 'APPROVED'))) AS state,
                customer_orders.created_at,
                customer_orders.updated_at,
                customer_orders.title,
                customer_orders.lending_terms_accepted,
                customer_orders.contact_details,
-               array_agg(reservations.id) AS reservation_ids,
+               ARRAY_AGG(reservations.id) AS reservation_ids,
                'customer_orders' AS origin_table
         FROM customer_orders
         JOIN orders ON orders.customer_order_id = customer_orders.id
