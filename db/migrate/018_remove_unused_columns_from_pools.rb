325e4660606c8ab4905f9882be44cdb033b5e2a6
Matus Kmit <matuskmit1@gmail.com>
2024-05-16 12:58:53 +0200

    chore: remove unused cols from pools

---
+++ db/migrate/018_remove_unused_columns_from_pools.rb
@@ -0,0 +1,14 @@
+class RemoveUnusedColumnsFromPools < ActiveRecord::Migration[6.1]
+  def change
+    %w(
+      color
+      contact_details
+      contract_description
+      contract_url
+      logo_url
+      opening_hours
+    ).each do |col|
+      remove_column :inventory_pools, col
+    end
+  end
+end
