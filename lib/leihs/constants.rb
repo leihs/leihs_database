f9b83722f3074c90614ef4e42a0213e8c997ec71
Matus Kmit <matuskmit1@gmail.com>
2024-07-15 11:56:30 +0200

    chore: user session cookie name constant

--- lib/leihs/constants.rb
+++ lib/leihs/constants.rb
@@ -1,6 +1,7 @@
 module Leihs
   module Constants
-    GENERAL_BUILDING_UUID = 'abae04c5-d767-425e-acc2-7ce04df645d1'
-    ALL_USERS_GROUP_UUID = '4dd87663-f731-5766-b97d-9494889ca66c'
+    GENERAL_BUILDING_UUID = 'abae04c5-d767-425e-acc2-7ce04df645d1'.freeze
+    ALL_USERS_GROUP_UUID = '4dd87663-f731-5766-b97d-9494889ca66c'.freeze
+    USER_SESSION_COOKIE_NAME = 'leihs-user-session'.freeze
   end
 end
