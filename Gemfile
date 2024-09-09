2137354d483a8ef53d6659f61e3c8da4c081c746
uvb <urs.vanbinsbergen@zhdk.ch>
2024-09-09 13:45:55 +0200

    chore: update to rails 7.0

--- Gemfile
+++ Gemfile
@@ -8,10 +8,10 @@ gem 'faker'
 gem 'mimemagic'
 gem 'money-rails'
 gem 'pg', platform: :mri
-gem 'pg_tasks', git: 'https://github.com/leihs/rails_pg-tasks'
+gem 'pg_tasks', git: 'https://github.com/leihs/rails_pg-tasks', branch: 'rails7'
 gem 'pry'
 gem 'psych', '~> 3' # https://github.com/rails/rails/issues/42528
-gem 'rails', '~> 6.1.0'
+gem 'rails', '~> 7.0.0'
 gem 'rspec'
 gem 'sequel'
 gem 'uuidtools'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,16 +29,16 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
+  # config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
