5f02a5906de45d49139c3e12ab5d83499e176f2d
mradl <manuel.radl@zhdk.ch>
2024-10-21 21:20:10 +0200

    act

--- lib/metadata_extractor.rb
+++ lib/metadata_extractor.rb
@@ -1,15 +1,15 @@
-# require 'exiftool_vendored'
+require 'exiftool_vendored'
 
 class MetadataExtractor
   attr_reader :data
 
   EXIFTOOL_CMD_LINE_OPTIONS = '-s -a -u -G1'
-  # Exiftool.command += " #{EXIFTOOL_CMD_LINE_OPTIONS}"
+  Exiftool.command += " #{EXIFTOOL_CMD_LINE_OPTIONS}"
 
   def initialize(file_path)
     @file_path = file_path
-    # @data = Exiftool.new(file_path)
-    @data = "test-data"
+    @data = Exiftool.new(file_path)
+    # @data = "test-data"
   end
 
   def to_hash
