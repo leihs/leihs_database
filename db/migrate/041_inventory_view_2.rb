b380d6ff0d1d75605d952b88bcd0c4e1d8183569
Matus Kmit <matuskmit1@gmail.com>
2025-05-23 17:21:08 +0200

    chore: inventory view 2

---
+++ db/migrate/041_inventory_view_2.rb
@@ -0,0 +1,45 @@
+class InventoryView2 < ActiveRecord::Migration[7.2]
+  def up
+    execute <<-SQL
+      DROP VIEW IF EXISTS inventory
+    SQL
+
+    execute <<-SQL
+      CREATE OR REPLACE VIEW inventory AS
+
+      SELECT id,
+             product,
+             version,
+             name,
+             type,
+             'models' AS origin_table,
+             is_package,
+             manufacturer,
+             NULL AS inventory_code,
+             NULL AS price,
+             NULL AS inventory_pool_id
+      FROM models
+
+      UNION
+
+      SELECT id,
+             product,
+             version,
+             name,
+             'Option' AS type,
+             'options' AS origin_table,
+             FALSE AS is_package,
+             manufacturer,
+             inventory_code,
+             price,
+             inventory_pool_id
+      FROM options
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP VIEW IF EXISTS inventory
+    SQL
+  end
+end
